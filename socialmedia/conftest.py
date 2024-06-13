3efe47d7213ad81d0677b8ba9528f9a3c1ad56ae
Erik <mirkovic.erik@gmail.com>
2024-06-13 17:28:06 +0200

    First commit

---
+++ socialmedia/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from socialmedia.users.models import User
+from socialmedia.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()

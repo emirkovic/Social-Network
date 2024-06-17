a154433ed7348c9170e6a93ba236b62b9e567295
Erik <mirkovic.erik@gmail.com>
2024-06-17 16:00:53 +0200

    fixes

--- socialmedia/users/tests/test_drf_urls.py
+++ socialmedia/users/tests/test_drf_urls.py
@@ -1,26 +1,24 @@
-import pytest
 from django.urls import resolve
 from django.urls import reverse
 
 from socialmedia.users.models import User
 
+# flake8: noqa
 
-@pytest.mark.django_db()
+
 def test_user_detail(user: User):
     assert (
-        reverse("users:detail", kwargs={"username": user.username})
-        == f"/users/{user.username}/"
+        reverse("api:user-detail", kwargs={"username": user.username})
+        == f"/api/users/{user.username}/"
     )
-    assert resolve(f"/users/{user.username}/").view_name == "users:detail"
+    assert resolve(f"/api/users/{user.username}/").view_name == "api:user-detail"
 
 
-@pytest.mark.django_db()
 def test_user_list():
     assert reverse("api:user-list") == "/api/users/"
     assert resolve("/api/users/").view_name == "api:user-list"
 
 
-@pytest.mark.django_db()
 def test_user_me():
     assert reverse("api:user-me") == "/api/users/me/"
     assert resolve("/api/users/me/").view_name == "api:user-me"
--- socialmedia/users/tests/test_swagger.py
+++ socialmedia/users/tests/test_swagger.py
@@ -4,7 +4,6 @@ import pytest
 from django.urls import reverse
 
 
-@pytest.mark.django_db()
 def test_swagger_accessible_by_admin(admin_client):
     url = reverse("api-docs")
     response = admin_client.get(url)
@@ -18,7 +17,6 @@ def test_swagger_ui_not_accessible_by_normal_user(client):
     assert response.status_code == HTTPStatus.FORBIDDEN
 
 
-@pytest.mark.django_db()
 def test_api_schema_generated_successfully(admin_client):
     url = reverse("api-schema")
     response = admin_client.get(url)

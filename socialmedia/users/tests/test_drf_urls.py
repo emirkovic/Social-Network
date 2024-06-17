25b772b7f51174a65684d07a97afdb60fa9accdb
Erik <mirkovic.erik@gmail.com>
2024-06-17 15:49:03 +0200

    fixes

--- socialmedia/users/tests/test_drf_urls.py
+++ socialmedia/users/tests/test_drf_urls.py
@@ -1,22 +1,26 @@
+import pytest
 from django.urls import resolve
 from django.urls import reverse
 
 from socialmedia.users.models import User
 
 
+@pytest.mark.django_db()
 def test_user_detail(user: User):
     assert (
-        reverse("api:user-detail", kwargs={"username": user.username})
-        == f"/api/users/{user.username}/"
+        reverse("users:detail", kwargs={"username": user.username})
+        == f"/users/{user.username}/"
     )
-    assert resolve(f"/api/users/{user.username}/").view_name == "api:user-detail"
+    assert resolve(f"/users/{user.username}/").view_name == "users:detail"
 
 
+@pytest.mark.django_db()
 def test_user_list():
     assert reverse("api:user-list") == "/api/users/"
     assert resolve("/api/users/").view_name == "api:user-list"
 
 
+@pytest.mark.django_db()
 def test_user_me():
     assert reverse("api:user-me") == "/api/users/me/"
     assert resolve("/api/users/me/").view_name == "api:user-me"

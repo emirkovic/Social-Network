9c42f5ec18db010298db3553f7e3c8fca950e052
Erik <mirkovic.erik@gmail.com>
2024-07-11 14:42:45 +0200

    Forms

--- socialmedia/social/forms.py
+++ socialmedia/social/forms.py
@@ -2,22 +2,15 @@ from django import forms
 
 from .models import Comment
 from .models import Post
-from .models import UserProfile
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ["image", "video", "text"]
+        fields = ["text", "image"]
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["text"]
-
-
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ["profile_image"]
--- socialmedia/social/views.py
+++ socialmedia/social/views.py
@@ -1,23 +1,57 @@
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from django.template import loader
+from django.shortcuts import redirect
+from django.shortcuts import render
 
+from .forms import CommentForm
+from .forms import PostForm
 from .models import Post
 
 
 def index(request):
+    if request.method == "POST":
+        post_form = PostForm(request.POST, request.FILES)
+        if post_form.is_valid():
+            post = post_form.save(commit=False)
+            post.user = request.user  # Assuming a user is logged in
+            post.save()
+            return redirect("social:index")
+    else:
+        post_form = PostForm()
+
     latest_post_list = Post.objects.filter(deleted=False).order_by("-created")
-    template = loader.get_template("pages/profile.html")
+
+    # Fetch the two most recent comments for each post
+    posts_with_comments = []
+    for post in latest_post_list:
+        comments = post.comments.filter(deleted=False).order_by("-created")[:2]
+        posts_with_comments.append((post, comments))
+
     context = {
-        "latest_post_list": latest_post_list,
+        "posts_with_comments": posts_with_comments,
+        "post_form": post_form,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "pages/profile.html", context)
 
 
 def detail(request, post_id):
-    post_detail = get_object_or_404(Post, pk=post_id, deleted=False)
-    template = loader.get_template("pages/post_detail.html")
+    post = get_object_or_404(Post, pk=post_id, deleted=False)
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.user = request.user  # Assuming a user is logged in
+            comment.save()
+            return redirect("social:detail", post_id=post_id)
+    else:
+        comment_form = CommentForm()
+
+    # Retrieve all comments for the detail page
+    comments = post.comments.filter(deleted=False).order_by("-created")
+
     context = {
-        "post_detail": post_detail,
+        "post_detail": post,
+        "comment_form": comment_form,
+        "comments": comments,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "pages/post_detail.html", context)
